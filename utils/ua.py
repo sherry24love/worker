9941775ebbc9ceae3c46ea97fd1e8f43b18a751f
gavin <sherry24love@163.com>
2019-10-16 00:09:06 +0800

    更新内容

---
+++ utils/ua.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+
+import os
+
+class ua:
+    """
+    ua 获取
+    """
+
+    current_dir = None
+
+
+    def __init__(self):
+        pass
+
+    def get_wechat_ua(self):
+        pass
+
+if __name__ == "__main__":
+    u = ua()
+    u.get_wechat_ua()
