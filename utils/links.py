70f21ded20cfc787a2aa1f173ca7962cc093f3a5
Huanghuaxin <huanghx@blizzmi.com>
2019-10-17 13:53:28 +0800

    更新应用

--- utils/links.py
+++ utils/links.py
@@ -2,42 +2,45 @@
 # -*- coding:utf-8 -*-
 
 import random
+import os
 
 
 class links:
 
     """ 开发模式 """
     debug = False
+    links = {}
 
     def __init__(self, debug=False):
         self.debug = debug
 
     def get_apps(self):
         """ 获取APP信息 """
-        return [
+        apps = [
             {
                 "name": "百家姓",
                 "code": "bjx"
-            },
-            {
-                "name": "金元宝",
-                "code": "jyb"
             }
         ]
+        self.current_dir = os.getcwd()
+        for app in apps:
+            code = app["code"]
+            self.links[code] = []
+            link_file = os.path.join(
+                self.current_dir, "data", "{}.txt".format(code))
+            fo = open(link_file, 'r')
+            for line in fo.readlines():  # 依次读取每行
+                line = line.strip()  # 去掉每行头尾空白
+                self.links[code].append(line)
+            fo.close()
+        return apps
 
     def get_links(self, index):
         """ 获取可用链接 """
-        url = [
-            "http://god.kzijhws.cn/ln/zlwegnonmmjwsiGfw.do?from=timeline",
-            "http://god.mnjkj.cn/wL/ujyiylncxjyqwbsiGhy.shtml?from=timeline",
-            "http://wsd.esezfv.cn/TepXK/bjdhqunppegsiGjlxl.do?from=timeline",
-            "http://wsd.grpmqw.cn/GdwT/pepjdlyhapgsiGjn.htm?from=timeline",
-            "http://wsd.gdwdvf.cn/loB/pfbvhvjnvzunsiGuh.htm?from=timeline",
-            "http://god.gnffrs.cn/GPGLdV/xoopnpupneesiGiq.shtml?from=timeline",
-            "http://nesw.rsjwnuuv.cn/wW/agugmmdxdlsiGikex.htm?from=timeline",
-            "http://nesw.rsjwnuuv.cn/mmFZOf/fxlyllnplxysiGcz.do?from=timeline",
-            "http://god.xgaeh.cn/HjPa/ugcyamlyydxsiGvgxf.htm?from=timeline"
-        ]
+        apps = self.get_apps()
+        app = apps[index]
+        code = app["code"]
+        url = self.links[code]
         if self.debug is True:
             url = [
                 "http://www.baidu.com"
